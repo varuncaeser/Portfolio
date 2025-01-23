26b7c3dff319ba378750884c267e3960d9054c55
Varun <varuvd2001@gmail.com>
2025-01-24 00:56:11 +0530

    initial changes

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text", {
+    strings: ["Python Developer" ,"Code Enthusiast","Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
